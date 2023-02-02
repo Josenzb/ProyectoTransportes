367f4249d13691ba1ba27218f7f3fcf0ac8bbbf5
nunezjose770 <nunezjose770@gmail.com>
2023-02-02 16:24:57 -0500

    First commit- Start project

---
+++ Transportes/src/test/java/co/com/sofka/Transportes/TransportesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.Transportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
